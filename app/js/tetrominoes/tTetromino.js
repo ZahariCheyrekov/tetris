63b7d081cfbbe62f4f935c57b645fa9254be2103
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-24 22:04:06 +0200

    Create t-shape tetromino

---
+++ app/js/tetrominoes/tTetromino.js
@@ -0,0 +1,28 @@
+import { GRID_WIDTH, START_POSITION } from "../constants.js";
+
+export const tTetromino = [
+    [
+        START_POSITION,
+        START_POSITION + 1,
+        START_POSITION + 2,
+        START_POSITION + 1 + GRID_WIDTH
+    ],
+    [
+        START_POSITION + GRID_WIDTH,
+        START_POSITION + 1,
+        START_POSITION + 1 + GRID_WIDTH,
+        START_POSITION + 1 + GRID_WIDTH * 2
+    ],
+    [
+        START_POSITION + 1,
+        START_POSITION + GRID_WIDTH,
+        START_POSITION + GRID_WIDTH + 1,
+        START_POSITION + GRID_WIDTH + 2,
+    ],
+    [
+        START_POSITION,
+        START_POSITION + GRID_WIDTH,
+        START_POSITION + GRID_WIDTH * 2,
+        START_POSITION + GRID_WIDTH + 1
+    ]
+];
