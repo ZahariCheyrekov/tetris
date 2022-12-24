84e372258671e364564c2eb3fc7a253204b54f07
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-24 22:28:08 +0200

    Create o-shape tetromino

---
+++ app/js/tetrominoes/oTetromino.js
@@ -0,0 +1,10 @@
+import { GRID_WIDTH, START_POSITION } from "../constants.js";
+
+export const oTetromino = [
+    [
+        START_POSITION,
+        START_POSITION + 1,
+        START_POSITION + GRID_WIDTH,
+        START_POSITION + GRID_WIDTH + 1,
+    ]
+];
