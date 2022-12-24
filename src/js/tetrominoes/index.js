ce7135c57bbaa263340b2a30820982fcb5da63a6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-24 23:33:13 +0200

    Create tetrominoes array and export it

---
+++ src/js/tetrominoes/index.js
@@ -0,0 +1,17 @@
+import { iTetromino } from './iTetromino.js';
+import { jTetromino } from './jTetromino.js';
+import { lTetromino } from './lTetromino.js';
+import { oTetromino } from './oTetromino.js';
+import { sTetromino } from './sTetromino.js';
+import { tTetromino } from './tTetromino.js';
+import { zTetromino } from './zTetromino.js';
+
+export const tetrominoesArray = [
+    iTetromino,
+    jTetromino,
+    lTetromino,
+    oTetromino,
+    sTetromino,
+    tTetromino,
+    zTetromino
+];
