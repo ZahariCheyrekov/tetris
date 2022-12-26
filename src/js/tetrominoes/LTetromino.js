e684483d72854e62bd241244c9d4debb6e0bf037
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-26 21:17:29 +0200

    Fix l-shaped tetromino rotation

--- src/js/tetrominoes/LTetromino.js
+++ src/js/tetrominoes/LTetromino.js
@@ -2,16 +2,16 @@ import { GRID_WIDTH, START_POSITION } from "../constants.js";
 
 export const lTetromino = [
     [
-        START_POSITION,
-        START_POSITION + GRID_WIDTH,
-        START_POSITION + GRID_WIDTH * 2,
-        START_POSITION + GRID_WIDTH * 2 + 1
+        START_POSITION + 1,
+        START_POSITION + 1 + GRID_WIDTH,
+        START_POSITION + 1 + GRID_WIDTH * 2,
+        START_POSITION + 2 + GRID_WIDTH * 2
     ],
     [
-        START_POSITION,
-        START_POSITION + 1,
-        START_POSITION + 2,
-        START_POSITION + GRID_WIDTH
+        START_POSITION + GRID_WIDTH,
+        START_POSITION + 1 + GRID_WIDTH,
+        START_POSITION + 2 + GRID_WIDTH,
+        START_POSITION + GRID_WIDTH * 2
     ],
     [
         START_POSITION,
