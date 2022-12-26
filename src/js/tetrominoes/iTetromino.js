6e767d3e72adfa089fed704d90df104dfe8b4cf2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-26 21:11:42 +0200

    Fix i-shaped tetromino rotation

--- src/js/tetrominoes/iTetromino.js
+++ src/js/tetrominoes/iTetromino.js
@@ -8,6 +8,18 @@ export const iTetromino = [
         START_POSITION + 1 + GRID_WIDTH * 3,
     ],
     [
+        START_POSITION - 1 + GRID_WIDTH,
+        START_POSITION + GRID_WIDTH,
+        START_POSITION + 1 + GRID_WIDTH,
+        START_POSITION + 2 + GRID_WIDTH,
+    ],
+    [
+        START_POSITION,
+        START_POSITION + GRID_WIDTH,
+        START_POSITION + GRID_WIDTH * 2,
+        START_POSITION + GRID_WIDTH * 3,
+    ],
+    [
         START_POSITION - 1,
         START_POSITION,
         START_POSITION + 1,
