fba4a22abc9d3e568f3e2491e3a2f38c2e97ce24
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-26 21:27:24 +0200

    Fix z-shaped tetromino rotation

--- src/js/tetrominoes/zTetromino.js
+++ src/js/tetrominoes/zTetromino.js
@@ -8,9 +8,21 @@ export const zTetromino = [
         START_POSITION + 2 + GRID_WIDTH
     ],
     [
+        START_POSITION + 2,
+        START_POSITION + 2 + GRID_WIDTH,
+        START_POSITION + 1 + GRID_WIDTH,
+        START_POSITION + 1 + GRID_WIDTH * 2,
+    ],
+    [
+        START_POSITION + GRID_WIDTH,
+        START_POSITION + 1 + GRID_WIDTH,
+        START_POSITION + 1 + GRID_WIDTH * 2,
+        START_POSITION + 2 + GRID_WIDTH * 2
+    ],
+    [
         START_POSITION + 1,
         START_POSITION + 1 + GRID_WIDTH,
         START_POSITION + GRID_WIDTH,
         START_POSITION + GRID_WIDTH * 2,
-    ],
+    ]
 ];
