c6042d9e00f221cce7d5183ef0f7c0a9de52e59b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-27 12:31:24 +0200

    Implement get tetromino to display funciton

--- src/js/tetrominoes/blocks.js
+++ src/js/tetrominoes/blocks.js
@@ -22,3 +22,7 @@ export const blocks = [
     // zTetromino
     [DISPLAY_WIDTH + 1, DISPLAY_WIDTH + START, DISPLAY_WIDTH * START + START, DISPLAY_WIDTH * START + 3],
 ];
+
+export function getTetreminoToDisplay(randomIndex) {
+    return blocks[randomIndex];
+}
