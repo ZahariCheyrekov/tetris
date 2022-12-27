f8b812b8d12f51a174270a3b5743151dff8d4b76
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-27 12:02:29 +0200

    Create blocks array

---
+++ src/js/tetrominoes/blocks.js
@@ -0,0 +1,24 @@
+import { START, DISPLAY_WIDTH } from "../constants.js";
+
+export const blocks = [
+    // iTetromino
+    [START, DISPLAY_WIDTH + START, DISPLAY_WIDTH * START + START, DISPLAY_WIDTH * 3 + START],
+
+    // jTetromino
+    [START, DISPLAY_WIDTH + START, DISPLAY_WIDTH * START + START, DISPLAY_WIDTH * START + 1],
+
+    // lTetromino
+    [1, DISPLAY_WIDTH + 1, DISPLAY_WIDTH * START + 1, DISPLAY_WIDTH * START + START],
+
+    // oTetromino
+    [1, START, DISPLAY_WIDTH + 1, DISPLAY_WIDTH + START],
+
+    // sTetromino
+    [1, DISPLAY_WIDTH + 1, DISPLAY_WIDTH + START, DISPLAY_WIDTH * START + START]
+
+    // tTetromino
+    [START, DISPLAY_WIDTH + 1, DISPLAY_WIDTH + START, DISPLAY_WIDTH * START + START],
+
+    // zTetromino
+    [DISPLAY_WIDTH + 1, DISPLAY_WIDTH + START, DISPLAY_WIDTH * START + START, DISPLAY_WIDTH * START + 3],
+];
