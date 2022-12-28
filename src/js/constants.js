6290e4133e4d1aac953fa2d539638d3ca3f021da
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-12-28 12:08:40 +0200

    Add tetromino colors constant

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,3 +3,7 @@ export const START_POSITION = 4;
 
 export const START = 2;
 export const DISPLAY_WIDTH = 4;
+
+export const TETROMINO_COLORS = [
+    '#FF399F', '#F0BB00', '#92F605', '#00F0FF', '#00ACFF', '#00ED87', '#FF3C00', '#00E2FF'
+];
